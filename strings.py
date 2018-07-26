3ae39ec80f97700616b9722c8be080f8764b6624
TushieeV <z5208092@student.unsw.edu.au>
2018-07-27 08:26:04 +1000

    Python 3rd exercise

--- strings.py
+++ strings.py
@@ -2,7 +2,10 @@ strings = ['This', 'list', 'is', 'now', 'all', 'together']
 sentence = ''
 
 for word in strings:
-    sentence = sentence + word + ' '
+    if (word != 'together'):
+        sentence = sentence + word + ' '
+    else:
+        sentence = sentence + word
 
 print(sentence)
 print(' '.join(strings))
